c82ea9ffa6145d7d207105c23ad68cbe1953b462
Conor Walsh <conor@foo.solutions>
2024-11-26 23:12:18 -0500

    solved day1 part 1

--- lib/2023/day1.ex
+++ lib/2023/day1.ex
@@ -5,9 +5,14 @@ defmodule Aoc.Y2023.D1 do
 
   def part1(input) do
     lines = helper(input)
-    lines
-    |> Enum.find(fn x -> is_int?(x) end)
-    |> IO.puts()
+    answer = lines
+    |> Enum.reduce(0, fn line, acc ->
+        chars = String.graphemes(line)
+        first = elem(Integer.parse(Enum.find(chars, fn x -> is_int?(x) end)), 0)
+        last = elem(Integer.parse(Enum.find(Enum.reverse(chars), fn x -> is_int?(x) end)), 0)
+        acc + 10 * first + last
+      end)
+    IO.puts(answer)
     :ok
   end
 
@@ -20,10 +25,9 @@ defmodule Aoc.Y2023.D1 do
   end
 
   defp is_int?(input) do
-    case input do
+    case Integer.parse(input) do
       {_, ""} -> true
       _ -> false
-
     end
   end
 end
