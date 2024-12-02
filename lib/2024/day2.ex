7025b62be73d196bcb551879a207eb4359a22821
Conor.Walsh <conor.walsh@blackstone.com>
2024-12-02 14:23:52 -0500

    solved day 2 part 2

--- lib/2024/day2.ex
+++ lib/2024/day2.ex
@@ -25,15 +25,8 @@ defmodule Aoc.Y2024.D2 do
     end
   end
 
-  defp safe_diff?(x, y, last) do
-    diff = y - x
 
-    if abs(diff) < 1 || abs(diff) > 3 || (diff > 0 && last < 0) || (diff < 0 && last > 0) do
-      {false, diff}
-    else
-      {true, diff}
-    end
-  end
+  ### Part 2
 
   def part2(input) do
     helper(input)
@@ -50,63 +43,23 @@ defmodule Aoc.Y2024.D2 do
   defp is_safe2?([], _, _, _), do: true
   defp is_safe2?([_ | [] ], _, _, _), do: true
 
-  defp is_safe2?([a | [b | rest] ], prev_diff, prev_diff2, removed) do
-    {safe, diff} = safe_diff2?(a, b, prev_diff)
-    if safe do
-      is_safe2?([b | rest], diff, prev_diff, removed)
-    else
-      unless removed do
-        unless is_safe2?([a | rest], prev_diff, prev_diff2, true) do
-          prev_number = a - prev_diff
-          # If prev_diff2 is 0 then we're at the second item of the list, and can potentially change direction
-          if prev_diff2 == 0 do
-            first_number = prev_number - prev_diff2
-            is_safe2?([first_number, prev_number, b] ++ rest, 0, 0, true)
-          else
-            is_safe2?([prev_number, b] ++ rest, prev_diff, prev_diff2, true)
-          end
+  defp is_safe2?([a | [b | rest] ], prev_diff, prev_diff2, modified) do
+    {safe, diff} = safe_diff?(a, b, prev_diff)
+    prev_number = a - prev_diff
+    [ c | _ ] = rest
+    cond do
+      safe -> is_safe2?([b | rest], diff, prev_diff, modified)
+      modified -> false
+      length(rest) == 0 -> true
+      is_safe2?([a , c], prev_diff, prev_diff2, true) -> is_safe2?([a , rest], prev_diff, prev_diff2, true)
+      prev_diff == 0 -> is_safe2?([b | rest], 0, 0, true)
+      prev_diff2 == 0 ->
+        unless is_safe2?([a, b] ++ rest, 0, 0, true) do
+          is_safe2?([prev_number, b] ++ rest, 0, 0, true)
         else
           true
         end
-      end
-      # false
-    end
-  end
-
-  # defp is_safe2?([a | [b | rest] ], last, removed, original_list) do
-  #   {safe, diff} = safe_diff2?(a, b, last)
-  #   if safe do
-  #     is_safe2?([b | rest], diff, removed)
-  #   else
-  #     unless removed do
-  #       unless is_safe2?([a | rest], last, true, original_list) do
-  #         prev_number = a - last
-  #         # If we're at the second item of the list we can potentially change direction
-  #         if start_over?(rest, original_list) do
-  #           is_safe2?([prev_number, b] ++ rest, 0, 0, true)
-  #         else
-  #           is_safe2?([prev_number, b] ++ rest, last, true, original_list)
-  #         end
-  #       else
-  #         true
-  #       end
-  #       # if length(rest) == 2, do: true
-  #     end
-  #     false
-  #   end
-  # end
-
-  # defp start_over(rest, original) do
-  #   length(rest) == length(original) - 3
-  # end
-
-  defp safe_diff2?(x, y, last) do
-    diff = y - x
-
-    if abs(diff) < 1 || abs(diff) > 3 || (diff > 0 && last < 0) || (diff < 0 && last > 0) do
-      {false, diff}
-    else
-      {true, diff}
+      true -> is_safe2?([prev_number, b] ++ rest, prev_diff, prev_diff2, true)
     end
   end
 
@@ -116,6 +69,18 @@ defmodule Aoc.Y2024.D2 do
   end
 
   defp parse_row(line) do
-    Enum.map(String.split(line), fn str -> parse_int(str) end)
+     Enum.map(String.split(line), fn str -> parse_int(str) end)
+  end
+
+  defp safe_diff?(x, y, prev_diff) do
+    diff = y - x
+    safe = cond do
+      abs(diff) < 1 -> false
+      abs(diff) > 3 -> false
+      diff > 0 && prev_diff < 0 -> false
+      diff < 0 && prev_diff > 0 -> false
+      true -> true
+    end
+    {safe, diff}
   end
 end
