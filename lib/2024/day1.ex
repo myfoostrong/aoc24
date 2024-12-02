331404489ab5e9ddb14ca403b908c49daaf9247b
trik <myfoostrong@gmail.com>
2024-12-01 23:08:16 -0500

    Solved day1 part1

--- lib/2024/day1.ex
+++ lib/2024/day1.ex
@@ -4,7 +4,20 @@ defmodule Aoc.Y2024.D1 do
   import Aoc.Util
 
   def part1(input) do
-    :ok
+    x = helper(input)
+    |> Enum.reduce([[], []], fn line, acc ->
+      for {str, list} <- Enum.zip(String.split(line, "   "), acc) do
+        insort(list, elem(Integer.parse(str), 0))
+      end
+    end)
+    Enum.zip(x)
+    |> Enum.reduce(0, fn {left, right}, acc ->
+      abs(left - right) + acc
+    end)
+  end
+
+  defp insort(list, input) do
+    Enum.sort([input | list])
   end
 
   def part2(input) do
